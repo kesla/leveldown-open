198d5d628c027e9daf5ac8511321c93a2b11b002
David Björklund <david.bjorklund@gmail.com>
2014-08-17 19:58:21 -0400

    don't do mkdir on existing directory

--- leveldown-open.js
+++ leveldown-open.js
@@ -1,26 +1,26 @@
 var fs = require('fs')
+
   , open = function (location, options, callback) {
       var subdir = location.split('/').slice(0, -1).join('/')
 
-      fs.exists(subdir, function (exists) {
-        if (!exists) {
-          callback(new Error(location + ': No such file or directory'))
-        } else if (options.createIfMissing === false || options.errorIfExists) {
-          fs.exists(location, function (exists) {
-            if (!exists && options.createIfMissing === false)
-              callback(
-                new Error(location + ' does not exist (createIfMissing is false)')
-              )
-            else if (exists && options.errorIfExists)
-              callback(
-                new Error(location + ' exists (errorIfExists is true)')
-              )
-            else
-              fs.mkdir(location, callback)
-          })
-        } else {
-          fs.mkdir(location, callback)
-        }
+      fs.exists(subdir, function (subdirExists) {
+        if (!subdirExists)
+          return callback(new Error(location + ': No such file or directory'))
+
+        fs.exists(location, function (exists) {
+          if (!exists && options.createIfMissing === false)
+            callback(
+              new Error(location + ' does not exist (createIfMissing is false)')
+            )
+          else if (exists && options.errorIfExists)
+            callback(
+              new Error(location + ' exists (errorIfExists is true)')
+            )
+          else if (!exists)
+            fs.mkdir(location, callback)
+          else
+            callback()
+        })
       })
     }
 
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-var test       = require('tape')
+var fs = require('fs')
+
+  , test       = require('tape')
   , testCommon = require('abstract-leveldown/testCommon')
   , openTest = require('abstract-leveldown/abstract/open-test')
   , open  = require('./leveldown-open')
@@ -22,3 +24,19 @@ openTest.setUp(test, testCommon)
 openTest.open(factory, test, testCommon)
 openTest.openAdvanced(factory, test, testCommon)
 openTest.tearDown(test, testCommon)
+
+test('open existing directory', function (t) {
+  var location = testCommon.location()
+    , filename = location + '/beep'
+
+  fs.mkdirSync(location)
+  fs.closeSync(fs.openSync(filename, 'w'))
+
+  open(location, {}, function (err) {
+    if (err)
+      return t.end(err)
+
+    t.equal(fs.existsSync(filename), true)
+    t.end()
+  })
+})
